7aa4455e99df7c0aac629bbdeb81bb9b8714420b
Marvin Schmoll <marvin.schmoll@physik.uni-freiburg.de>
2025-12-17 17:21:19 +0100

    Added possibility for llower limit in peak finder

--- vmi_analysis_library.py
+++ vmi_analysis_library.py
@@ -545,7 +545,7 @@ class VMI_scan():
     
         
     
-    def energy_scale(self, max_pixel=550, peak_distance=2/3,
+    def energy_scale(self, min_pixel=0, max_pixel=550, peak_distance=2/3,
                      height=0.1, prominence=0.1, width=5):
         """
         Performs curve fit to determine energy axis.
@@ -555,7 +555,11 @@ class VMI_scan():
         Parameters
         ----------
         max_pixel : int, optional
-            Pixel up to which peaks can will be recognized as harmonics/sidebands. 
+            Pixel starting from which peaks will be recognized as harmonics/sidebands. 
+            The default is 0, i.e. from the beginning.
+
+        max_pixel : int, optional
+            Pixel up to which peaks will be recognized as harmonics/sidebands. 
             The default is 550.
             
         peak_distance : int or float, optional
@@ -595,9 +599,10 @@ class VMI_scan():
         def velocity(n, a, b):   # n, b in [harm. orders]; a in [samples^2/harm. order]
             return np.sqrt(a * (n+b))   # output in [samples]
         
-        peaks, properties = scipy.signal.find_peaks(self.speed_distribution[0:max_pixel], 
+        peaks, properties = scipy.signal.find_peaks(self.speed_distribution[min_pixel:max_pixel], 
                                                     height=height, prominence=prominence,
                                                     width=width)
+        peaks = peaks + min_pixel
         
         plt.figure(num='Speed distribution', clear=True)
         plt.plot(self.speed_distribution)
